327890a958d06b3000263969e4f8957728a71dc4
Mnigos <makowskiigor@gmail.com>
2022-09-16 00:27:16 +0200

    refactor(scripts/deploy): prepare for new contracts

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,25 +1,22 @@
 import { ethers } from 'hardhat'
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000)
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS
+  const [deployer] = await ethers.getSigners()
+  const deployerBalance = await deployer.getBalance()
 
-  const lockedAmount = ethers.utils.parseEther('1')
+  console.log('Deploying contracts with the account:', deployer.address)
+  console.log('Account balance:', deployerBalance.toString())
 
-  const Lock = await ethers.getContractFactory('Lock')
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount })
+  const NFT = await ethers.getContractFactory('NFT')
+  const Marketplace = await ethers.getContractFactory('Marketplace')
 
-  await lock.deployed()
-
-  console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  )
+  await Marketplace.deploy(1)
+  await NFT.deploy()
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch(error => {
+try {
+  main()
+} catch (error) {
   console.error(error)
   process.exitCode = 1
-})
+}
